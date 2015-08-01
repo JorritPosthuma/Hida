94b1c05fd4f2665f57cc7281c09a5ab495a7d9ec
Jorrit Posthuma <dev@nolemmings.nl>
2015-08-01 08:51:13 +0200

    Extract static images

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
-var path = require("path");
-var webpack = require("webpack");
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: ['webpack/hot/dev-server', './src/logic/main'],
   output: {
     path: './app',
     filename: 'logic/main.js'       
-  },
-  resolve: {
   },
   module: {
     loaders: [
@@ -22,19 +20,19 @@ module.exports = {
         loader: 'style!css!sass'
       }, {
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff"
+        loader: 'url?limit=10000&mimetype=application/font-woff'
       }, {
         test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff"
+        loader: 'url?limit=10000&mimetype=application/font-woff'
       }, {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/octet-stream"
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
       }, {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file"
+        loader: 'file'
       }, {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=image/svg+xml"
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
       }, {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: 'url?limit=10000'
@@ -42,16 +40,16 @@ module.exports = {
     ]
   },
   resolve: {
-    root: [path.join(__dirname, "app/lib/bower")],
+    root: [path.join(__dirname, 'app/lib/bower')],
     extensions: ['', '.js', '.json', '.coffee'] 
   },
   plugins: [
     new webpack.ResolverPlugin(
-      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
     )
   ],
   devServer: {
-    contentBase: "./app",
+    contentBase: './app',
     port: 5000
   }
 };
